619a132b8918b017a5c425099200eedd542a25c9
holy star <holy1017@naver.com>
2019-07-09 13:36:54 +0900

    Update perl.pl

--- perl.pl
+++ perl.pl
@@ -5,6 +5,30 @@
 
 watch -n 5 ./perl.pl
 
-print "test￦n";
+print "test\n";
+
+
 
 # 0~9
+for ( $i=0;$i<9;$i++ ){
+ $arr[$i]=rand(7);
+ print "$i : $arr[$i]\n";
+}
+
+$#arr=4;
+
+for ( $i=0;$i<9;$i++ ){
+ print "$i : $arr[$i]\n";
+}
+
+$#arr=7;
+
+for ( $i=0;$i<9;$i++ ){
+ print "$i : $arr[$i]\n";
+}
+
+# a234b789
+for( "a",2..4,"b",7..9){
+ print;
+}
+
