6216c6c4c666f90a0009dc85c116c9bc26840f13
holy star <holy1017@naver.com>
2019-08-01 22:26:17 +0900

    Create while-read-function.sh

---
+++ while-read-function.sh
@@ -0,0 +1,22 @@
+cat <<EOF> List.txt
+1 "3 1" 21
+2 "3 2" 22
+EOF
+cat List.txt
+
+function f_echo(){
+    for item in "$@" ;do
+        echo $item
+    done
+}
+
+while read line; do
+    # ""를 인식처리
+    array=( )
+    while IFS= read -r -d ''; do
+      array+=( "$REPLY" )
+    done < <(xargs printf '%s\0' <<<"$line")
+
+    f_echo "${array[@]}"
+
+done < List.txt
