224393c6519e758a5d330ed97374fbe4d6f34f72
holy star <holy1017@naver.com>
2019-08-01 21:14:10 +0900

    Update function.sh

--- function.sh
+++ function.sh
@@ -15,46 +15,5 @@ wait `jobs -l | awk '{print $2}'`
 
 echo ${arr[*]}
 #----------
-cat <<EOF> List_entries.txt
-Item1
-Item 2 
-'Item 3'
-"Item 4"
-Item 7 : *
-"Item 6 : * "
-"Item 6 : *"
-Item 8 : $PWD
-'Item 8 : $PWD'
-"Item 9 : $PWD"
-EOF
 
-List=$(cat List_entries.txt)
-echo $List
-echo '$List'
-echo "$List"
-echo ${List[*]}
-echo '${List[*]}'
-echo "${List[*]}"
-echo ${List[@]}
-echo '${List[@]}'
-echo "${List[@]}"
-
-
-function f_echo(){
- echo ${*}
- echo ${@}
-for item in (()) ;do
- echo $item
-done
-
-}
-echo $List
-echo '$List'
-echo "$List"
-echo ${List[*]}
-echo '${List[*]}'
-echo "${List[*]}"
-echo ${List[@]}
-echo '${List[@]}'
-echo "${List[@]}"
 
