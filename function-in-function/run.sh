c353a71dbd263ce744ff0e937ff899486ebe7969
holy star <holy1017@naver.com>
2019-08-13 10:07:22 +0900

    Update run.sh

--- function-in-function/run.sh
+++ function-in-function/run.sh
@@ -1,1 +1,25 @@
+echo "a1 a2 a3
+#test 865 one
+y7 y6 y5" > test-f_test-no1.lst
 
+function f_loop(){
+ while read line; do
+  echo "f_loop : $line"
+  if [[ $line =~ ^#.* ]];
+  then
+   continue;
+  else
+   $1 ${line[@]};
+  fi
+ done < ./test-$1-$2.lst
+}
+
+function f_test(){
+ echo "f_test : $*"
+ for item in $@;
+ do
+  echo "$echo"
+ done
+}
+
+f_loop f_test no1
