fecddcb5d7070bb0739f4da175fb54ecf463c7c9
holy star <holy1017@naver.com>
2019-10-23 16:42:51 +0900

    Create while-list.sh

---
+++ while-list.sh
@@ -0,0 +1,16 @@
+function f_list(){
+ echo "=== $FUNCNAME ST ==="
+ 
+ while read line; do
+  echo "line:$line:"
+  v_rgx="^ *$"
+  fi [[ $line =~ ^#.* ]] | [[ $line =~ $v_rgx ]] ;
+  then
+  
+  else
+  
+  fi
+ done < $(ls $2)
+ 
+ echo "=== $FUNCNAME ST ==="
+}
