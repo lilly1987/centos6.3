cd0afaaf30bcaee746fc4993ae372e82e4f5bbfc
holy star <holy1017@naver.com>
2019-12-27 12:01:02 +0900

    Update deploy.sh
    t

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -21,6 +21,32 @@ EOF
  } &
 done < $(ls $t_pwd/$t_name-*.dlst)
 
+ls -d */
+for dir in */; do
+  echo "$dir"
+done
+
+while read line ; do
+ {
+  if [[ $line =~ ^#.* ]] ; then continue ; fi
+  arr=($line);
+  nc -zw1 ${arr[3]} 22 > /dev/null ;
+  if [ $? -eq 0 ] ;
+  then
+   sftp ${arr[2]}@${arr[3]} <<EOF # 2> /dev/null
+mkdir ~/$t_base
+lcd $t_pwd
+cd ~/$t_base
+put *.*
+EOF
+   else
+    echo "can't connet"
+   fi
+ } &
+done < $(ls $t_pwd/$t_name-*.dlst)
+
+
+
 wait `jobs -l | awk '$3 ~ /Running/{print $2}'`
 
 echo "end"
