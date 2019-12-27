70a98d1f434e4af0e655a6b56b7b0feb6f70e399
holy star <holy1017@naver.com>
2019-12-27 14:00:25 +0900

    Update deploy.sh

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,11 +1,14 @@
 t_pwd="$(cd "$(dirname "$0")" && pwd)/"
 t_base="$(basename $t_pwd)/"
 t_name="test"
+t_dlst="$t_name-deploy-*.dlst"
+t_rgx="^ *$"
 
-while read line ; do
+
+while read v_line ; do
  {
-  if [[ $line =~ ^#.* ]] ; then continue ; fi
-  arr=($line);
+  if [[ $v_line =~ ^#.* ]] || [[ $v_line =~ $t_rgx ; then continue ; fi
+  arr=($v_line);
   nc -zw1 ${arr[3]} 22 > /dev/null ;
   if [ $? -eq 0 ] ;
   then
@@ -24,7 +27,7 @@ done < $(ls $t_pwd/$t_name-*.dlst)
 ls -d */
 for dir in */; do
   echo "$dir"
-done
+
 
 while read line ; do
  {
@@ -45,7 +48,7 @@ EOF
  } &
 done < $(ls $t_pwd/$t_name-*.dlst)
 
-
+done
 
 wait `jobs -l | awk '$3 ~ /Running/{print $2}'`
 
