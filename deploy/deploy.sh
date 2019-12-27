bc6640691376cd8dcb415cfd520b828f222a9dfe
holy star <holy1017@naver.com>
2019-12-27 11:27:47 +0900

    Update deploy.sh

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,14 +1,15 @@
 t_pwd="$(cd "$(dirname "$0")" && pwd)/"
+t_base="$(basename $t_pwd)"
 
 while read line ; do
  {
   if [[ $line =~ ^#.* ]] ; then continue ; fi
   arr=($line);
-  nc -zw1 ${arr[2]} 22 > /dev/null ;
+  nc -zw1 ${arr[3]} 22 > /dev/null ;
   if [ $? -eq 0 ] ;
   then
-   sftp user@192.168.0.1 <<EOF 2> /dev/null
-lcd /home/user/script
+   sftp $${arr[3]} <<EOF # 2> /dev/null
+lcd /home/user/
 cd /home/user/script
 put *.*
 EOF
@@ -16,7 +17,7 @@ EOF
     echo "can't connet"
    fi
  } &
-done < server.lst
+done < 
 
 wait `jobs -l | awk '$3 ~ /Running/{print $2}'`
 
