65389f439fefded5fee311ee2480f2134cda6970
holy star <holy1017@naver.com>
2019-12-27 11:22:53 +0900

    Create deploy.sh

---
+++ deploy/deploy.sh
@@ -0,0 +1,23 @@
+t_pwd="$(cd "$(dirname "$0")" && pwd)/"
+
+while read line ; do
+ {
+  if [[ $line =~ ^#.* ]] ; then continue ; fi
+  arr=($line);
+  nc -zw1 ${arr[2]} 22 > /dev/null ;
+  if [ $? -eq 0 ] ;
+  then
+   sftp user@192.168.0.1 <<EOF 2> /dev/null
+lcd /home/user/script
+cd /home/user/script
+put *.*
+EOF
+   else
+    echo "can't connet"
+   fi
+ } &
+done < server.lst
+
+wait `jobs -l | awk '$3 ~ /Running/{print $2}'`
+
+echo "end"
