a32b81403c18bd8af9863fcc528a189a5cf46bef
lilly <lilly1987@naver.com>
2020-10-03 20:53:53 +0900

    Update function.sh

--- function.sh
+++ function.sh
@@ -16,4 +16,20 @@ wait `jobs -l | awk '{print $2}'`
 echo ${arr[*]}
 #----------
 
+# https://hashcode.co.kr/questions/1893/%EB%A6%AC%EB%88%85%EC%8A%A4-%ED%99%98%EA%B2%BD%EB%B3%80%EC%88%98-%EC%84%A4%EC%A0%95%ED%95%A0-%EB%95%8C-env-set-export-declare
+# 변수 삭제
+unset test1
+test1=''
+f_test0 () {
+  echo "=== $FUNCNAME ST ==="
+  # "test1=$2$3" 라는 명령어를 eval가 실행
+  eval "$1=\$2\$3"
+  echo $1
+  eval "echo \$$1"
+  echo "=== $FUNCNAME ED ==="
+}
+f_test0 test1 test2 test3
+echo $test1
 
+# 변수 확인
+set | grep test
